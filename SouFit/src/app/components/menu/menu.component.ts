e462a9bd7f5f91f548e6586f76de6e4f30f9e5e9
Joshua <joshuavilla146@gmail.com>
2025-10-21 00:34:47 -0300

    Componente

--- SouFit/src/app/components/menu/menu.component.ts
+++ SouFit/src/app/components/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -6,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.scss'],
 })
 export class MenuComponent  implements OnInit {
+  router: any;
 
   constructor() { }
 
   ngOnInit() {}
+
+  volverHome() {
+    console.log("Navegando a Home");
+    this.router.navigate(['/home']);
+  }
+  buscar() {
+    console.log("buscar");
+    this.router.navigate(['/buscar']);
+  }
+  rutinas() {
+    console.log("Navegando a Rutinas");
+    this.router.navigate(['/rutinas']);
+  }
+  mensajeria(){
+    console.log("Navegando a Mensajería");
+    this.router.navigate(['/mensajeria']);
+  }
+  perfil(){
+    console.log("Navegando a Perfil");
+    this.router.navigate(['/perfil']);
+  }
 
 }
