7ae2c10e4941ebf6398d11ee59a032ef24d5e9d7
German-Oses <172307977+German-Oses@users.noreply.github.com>
2025-10-28 17:08:59 -0300

    Instrucciones

---
+++ SouFit/.angular/cache/20.3.3/app/vite/deps/md.transition-D8TeJ_Pz-DW5CUGKO.js
@@ -0,0 +1,12 @@
+import {
+  mdTransitionAnimation
+} from "./chunk-7UQAHEP3.js";
+import "./chunk-ZBHXYRST.js";
+import "./chunk-2WKSIOYF.js";
+import "./chunk-N3MBJFEE.js";
+import "./chunk-LCMILTBF.js";
+import "./chunk-UEKR4QPM.js";
+import "./chunk-EAE2VPRF.js";
+export {
+  mdTransitionAnimation
+};
---
+++ SouFit/.angular/cache/20.3.3/app/vite/deps/status-tap-pATNXEtr-GL7K2UDE.js
@@ -0,0 +1,50 @@
+import {
+  findClosestIonContent,
+  scrollToTop
+} from "./chunk-6SPHPIOA.js";
+import {
+  componentOnReady
+} from "./chunk-N3MBJFEE.js";
+import {
+  readTask,
+  writeTask
+} from "./chunk-UEKR4QPM.js";
+import {
+  __async
+} from "./chunk-EAE2VPRF.js";
+
+// node_modules/@ionic/core/dist/esm/status-tap-pATNXEtr.js
+var startStatusTap = () => {
+  const win = window;
+  win.addEventListener("statusTap", () => {
+    readTask(() => {
+      const width = win.innerWidth;
+      const height = win.innerHeight;
+      const el = document.elementFromPoint(width / 2, height / 2);
+      if (!el) {
+        return;
+      }
+      const contentEl = findClosestIonContent(el);
+      if (contentEl) {
+        new Promise((resolve) => componentOnReady(contentEl, resolve)).then(() => {
+          writeTask(() => __async(null, null, function* () {
+            contentEl.style.setProperty("--overflow", "hidden");
+            yield scrollToTop(contentEl, 300);
+            contentEl.style.removeProperty("--overflow");
+          }));
+        });
+      }
+    });
+  });
+};
+export {
+  startStatusTap
+};
+/*! Bundled license information:
+
+@ionic/core/dist/esm/status-tap-pATNXEtr.js:
+  (*!
+   * (C) Ionic http://ionicframework.com - MIT License
+   *)
+*/
+//# sourceMappingURL=status-tap-pATNXEtr-GL7K2UDE.js.map
