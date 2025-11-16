0710f6812acf3ee8bf02e67f74f4b330d2a11369
German-Oses <172307977+German-Oses@users.noreply.github.com>
2025-11-16 11:07:01 -0300

    fixed bug in ubicacion.js

--- SouFit/BackEnd/config/db.js
+++ SouFit/BackEnd/config/db.js
@@ -2,18 +2,22 @@ const { Pool } = require('pg');
 const path = require('path');
 const dotenv = require('dotenv');
 
-
 dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
-const pool = new Pool({
+const config = {
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
-});
+  ssl: {
+    rejectUnauthorized: false
+  }
+};
+
+const pool = new Pool(config);
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
-  pool: pool,
+  pool: pool
 };
